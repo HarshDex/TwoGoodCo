63dedb8e0ad735e79a32c48cd933a1cac2834102
HarshDex <nature4917@gmail.com>
2023-09-26 05:20:55 +0530

    fixed minor bug

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
+// ? this is the code for cursor tracking 
 let main = document.querySelector(".main-container");
 let cursor = document.querySelector(".cursor");
 main.addEventListener("mousemove",(dets)=>{
-    cursor.style.left = dets.clientX - 36+ "px";
-    cursor.style.top = dets.clientY - 35 + "px"
+    cursor.style.left = dets.clientX  -35 +  "px";
+    cursor.style.top = dets.clientY  -36   + "px"
 })  
 
-//? event listner for card section
-// let image = document.querySelectorAll(".page2-image-container-image img");
-// image.forEach((elem)=>{
-//     elem.addEventListener("mousemove",(dets)=>{
-//         cursor.style.width = "100px";
-//         cursor.style.height = "100px";
-//         cursor.style.color = "black";
-//         cursor.innerHTML = "Play"
-        
-//     })
-// })
-// image.forEach((elem)=>{
-//     elem.addEventListener("mouseleave",(dets)=>{
-//         cursor.style.width = "0px";
-//         cursor.style.height = "0px";
-//         cursor.style.color = "transparent";
-//         cursor.innerHTML = ""
-//     })
-// })
+//? event lisner for video section
+// ?(mouse enter)
 let video = document.querySelector(".page1-video-section video");
 video.addEventListener("mousemove",(dets)=>{
     cursor.style.width = "100px";
@@ -31,9 +15,12 @@ video.addEventListener("mousemove",(dets)=>{
     cursor.innerHTML = "Play"
     cursor.style.color  = "white";
 })
+// ?(mouse leave)
 video.addEventListener("mouseleave",(dets)=>{
     cursor.style.width = "0px";
     cursor.style.height = "0px";
     cursor.style.color = "transparent";
     cursor.innerHTML = ""
 })
+
+
